475931ed92d6b5ecd51dbfe4adfee67a3597da9b
anaumovich <naumovich.anton@gmail.com>
2018-09-28 00:44:43 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+    let arr = equation.split(' ');
+    let result = [];
+    let A = arr[0];
+    let B = arr[3] + arr[4];
+    let C = arr[7] + arr[8];
+    let D = Math.pow(B,2) - 4 * A * C;
+
+
+    if (D > 0) {
+        result[0] = Math.round(( -B + Math.sqrt(D) )/(2 * A));
+        result[1] = Math.round(( -B - Math.sqrt(D) )/(2 * A));
+     }
+
+    result.sort(sortMass);
+
+    return result;
+
+};
+
+function sortMass(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
 }
